53f20dbefc2809e71489ebc37adf14317f9b8cd4
stormbringer951 <stormbringer951@gmail.com>
2022-01-02 21:38:27 +0000

    Update AdjustableSkillThresholdsModPlugin.java

--- src/AdjustableSkillThresholds/AdjustableSkillThresholdsModPlugin.java
+++ src/AdjustableSkillThresholds/AdjustableSkillThresholdsModPlugin.java
@@ -11,10 +11,9 @@ import com.fs.starfarer.api.impl.campaign.skills.DerelictContingent;
 public class AdjustableSkillThresholdsModPlugin extends BaseModPlugin {
     private static final String FIGHTER_BAYS_THRESHOLD = "carrier_group_fighter_uplink_fighter_bay_threshold";
     private static final String CREW_TRAINING_FLUX_REGULATION_OP = "crew_training_flux_regulation_op_threshold";
-    private static final String WEAPONS_DRILLS_OP = "tactical_drills_op_threshold";
+    private static final String TACTICAL_DRILLS_OP = "tactical_drills_op_threshold";
     private static final String CONTAINMENT_PROCEDURES_FIELD_REPAIRS_OP = "containment_procedures_field_repairs_op_threshold";
-    // private static final String AUXILIARY_SUPPORT_OP = "auxiliary_support_op_threshold";
-    private static final String PHASE_CORPS_OP = "phase_coil_tuning_op_threshold";
+    private static final String PHASE_COIL_TUNING_OP = "phase_coil_tuning_op_threshold";
     private static final String AUTOMATED_SHIPS_OP = "automated_ships_op_threshold";
     private static final String CARGO_THRESHOLD = "bulk_transport_cargo_threshold";
     private static final String FUEL_THRESHOLD = "bulk_transport_fuel_threshold";
@@ -41,10 +40,9 @@ public class AdjustableSkillThresholdsModPlugin extends BaseModPlugin {
     public void onApplicationLoad() {
         BaseSkillEffectDescription.FIGHTER_BAYS_THRESHOLD = getSettingsInt(FIGHTER_BAYS_THRESHOLD);
         BaseSkillEffectDescription.OP_THRESHOLD = getSettingsInt(CREW_TRAINING_FLUX_REGULATION_OP);
-        BaseSkillEffectDescription.OP_LOW_THRESHOLD = getSettingsInt(WEAPONS_DRILLS_OP);
+        BaseSkillEffectDescription.OP_LOW_THRESHOLD = getSettingsInt(TACTICAL_DRILLS_OP);
         BaseSkillEffectDescription.OP_ALL_THRESHOLD = getSettingsInt(CONTAINMENT_PROCEDURES_FIELD_REPAIRS_OP);
-        // BaseSkillEffectDescription.MILITARIZED_OP_THRESHOLD = getSettingsInt(AUXILIARY_SUPPORT_OP);
-        BaseSkillEffectDescription.PHASE_OP_THRESHOLD = getSettingsInt(PHASE_CORPS_OP);
+        BaseSkillEffectDescription.PHASE_OP_THRESHOLD = getSettingsInt(PHASE_COIL_TUNING_OP);
         BaseSkillEffectDescription.AUTOMATED_POINTS_THRESHOLD = getSettingsInt(AUTOMATED_SHIPS_OP);
         BulkTransport.CARGO_CAPACITY_THRESHOLD = getSettingsInt(CARGO_THRESHOLD);
         BulkTransport.FUEL_CAPACITY_THRESHOLD = getSettingsInt(FUEL_THRESHOLD);
